b19116f28628fd37d0106e3ae6acbebed5afa33a
MJbae <bmj5208@gmail.com>
2020-07-28 09:21:35 +0900

    refactor: 각 메소드에 주석 표시하여 가독성 향성

--- TodoList/TodoListBasic/TodoList.js
+++ TodoList/TodoListBasic/TodoList.js
@@ -74,7 +74,9 @@ function TodoApp(todoList, elementId) {
           : `<li>${text}<button type="text" name="deleteList"></button></li>`;
       })
       .join('');
-    this.listEditor();
+    // TODO: 왜 render() 내 listEditor()가 없으면 추가, 삭제, 삭선 생성 기능이 작동을 안할까?
+    //render로 DOM 객체를 브라우저 상에 표현만 하므로, 표현된 객체 하나하나에 이벤트를 걸어주는 작업이 추가적으로 필요함
+    // 그 작업을 listEditor가 수행. 
   };
 
   this.setState = (nextData) => {
@@ -86,17 +88,20 @@ function TodoApp(todoList, elementId) {
 
   this.listEditor = () => {
     // addList
-    console.log('start addList')
+    console.log('start listEditor')
     this.elelmentId
       .querySelector('input[name="addList"]')
       .addEventListener('keyup', (event) => {
+        console.log('sensing keyup')
         // 작성한 목록이 있고, enter를 눌렀을 때 add list 후 reset
         if (event.currentTarget.value && event.keyCode === 13) {
+          console.log('sensing enter')
           const addData = {
             text: event.currentTarget.value,
             isCompleted: false,
           };
-          // TODO: 어떤 의미에서 immutable ?
+          // 기존 배열인 this.todos에 접근하여 값을 변경하는 것이 아니라 immutableList에 
+          // 기존 값과 추가된 값을 더하여 원본 값에 변경을 주지 않음. 따라서 immutable하게 접근함
           const immutableList = [...this.todos, addData];
           this.setState(immutableList);
           event.currentTarget.value = '';
@@ -104,9 +109,11 @@ function TodoApp(todoList, elementId) {
       });
 
     // removeList
+    // render 후에 listEditor를 호출하는 이유는 변경사항 발생에 따라 DOM 각 요소에 idx를 재할당하기 위해
     const buttons = this.listDom.querySelectorAll('button[name="deleteList"]');
     buttons.forEach((button, idx) => {
       button.addEventListener('click', () => {
+        console.log('start removeList')
         event.stopPropagation();
         this.todos.splice(idx, 1);
         this.render();
@@ -116,11 +123,11 @@ function TodoApp(todoList, elementId) {
       });
     });
 
-    //TODO: 동작원리 이해하기 
     // checkList(isCompleted)
     const lists = this.listDom.querySelectorAll('li');
     lists.forEach((list, idx) => {
       list.addEventListener('click', () => {
+        console.log('start checkList')
         var isCompletedOrigin = this.todos[idx].isCompleted;
         this.todos[idx].isCompleted = !isCompletedOrigin;
         this.render();
@@ -135,6 +142,7 @@ function TodoApp(todoList, elementId) {
     }
     CheckDataFormat(todoList);
     this.render();
+    // this.listEditor();
   } catch (error) {
     // 에러를 catch하여 해당 엘리먼트에 error UI를 구현한다.
     renderErrorNotification(error, elementId);
