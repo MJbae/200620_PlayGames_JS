5bf88a6e2d4a4eeb1f52565aeb8ff3e1e61e060c
MJbae <bmj5208@gmail.com>
2020-07-28 12:52:28 +0900

    refactor: 각종 components를 components 폴더로 이동 및 function 선언 함수로 변경

---
+++ TodoList/TodoListUpgrade/components/TodoInput.js
@@ -0,0 +1,46 @@
+function TodoInput({ elementId, listEditor }) {
+  // section 태그 접근
+  this.elementDom = document.querySelector(`#${elementId}`);
+  // section 내 ul 태그 접근
+  this.listDom = this.elementDom.querySelector('.content');
+  this.listEditor = listEditor;
+  // input에 이벤트 검
+  this.render = () => {
+    this.elementDom
+      .querySelector('input[name="addList"]')
+      .addEventListener('keyup', (event) => {
+        console.log('sensing keyup')
+        // 작성한 목록이 있고, enter를 눌렀을 때 add list 후 reset
+        if (event.currentTarget.value && event.keyCode === 13) {
+          console.log('sensing enter')
+          const addData = {
+            text: event.currentTarget.value,
+            isCompleted: false,
+          };
+          this.listEditor.add(addData);
+          event.currentTarget.value = '';
+        }
+      })
+
+    // deleteList buttons에 이벤트를 생성
+    const buttons = this.listDom.querySelectorAll('button[name="deleteList"]');
+    buttons.forEach((button, idx) => {
+      button.addEventListener('click', () => {
+        console.log('start removeList')
+        // TODO: 아래 event는 어디서 왔나?
+        event.stopPropagation();
+        this.listEditor.remove(idx);
+      });
+    });
+
+    const lists = this.listDom.querySelectorAll('li');
+    lists.forEach((list, idx) => {
+      list.addEventListener('click', () => {
+        this.listEditor.check(idx);
+      });
+    });
+  };
+  this.render();
+}
+
+export default TodoInput;
---
+++ TodoList/TodoListUpgrade/components/TodoList.js
@@ -0,0 +1,25 @@
+// TODO: components 폴더로 각종 components 파일을 옮김에 따라 각 컴포넌트 내부에 render 부분을 추가함. 왜??
+
+// TodoList.js에는 렌더링 시 리스트를 그려주는 부분만 남기고 나머지는 App.js로 이동
+
+function TodoList(todoList, listDom) {
+  this.todoList = todoList;
+  this.listDom = listDom;
+  this.render = () => {
+    this.listDom.innerHTML = todoList
+      .map((todo) => {
+        // TODO: 아래 배열 구문은 어떤 쓰임?
+        const { text, isCompleted = false } = todo;
+
+        if (!text) {
+          return;
+        }
+        return isCompleted
+          ? `<li><s>${text}</s><button type="text" name="deleteList"></button></li>`
+          : `<li>${text}<button type="text" name="deleteList"></button></li>`;
+      })
+      .join('');
+  }
+};
+
+export default TodoList;
